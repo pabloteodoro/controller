52bb6583cb8b3276c713fc5f5607da527e586fe2
Pablo Teodoro <pabloteadoro123@gmail.com>
2025-05-07 22:29:21 -0300

    install next auth and install prisma with mongodb, create files for autentication

--- controlldev/src/components/header/index.tsx
+++ controlldev/src/components/header/index.tsx
@@ -7,12 +7,10 @@ export function Header() {
             <div className="w-full flex items-center justify-between max-w-7xl mx-auto">
             <Link href="/">
             <h1 className="font-bold text-2xl pl-1 hover:tracking-widest duration-300">
-                <span className="text-blue-500">DEV
-
-                </span>
-                CONTROLE
+                <span className="text-blue-500">DEV</span> CONTROLE
             </h1>
             </Link>
+            
             <div className="flex items-baseline gap-4">
                 <Link href="/dashboard">
                 <FiUser size={26} color="#4b5563"/>
