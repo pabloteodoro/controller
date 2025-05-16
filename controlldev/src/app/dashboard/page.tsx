c70921a627f5620ced66368082f1aafe6b5ea5b5
Pablo Teodoro <pabloteadoro123@gmail.com>
2025-05-16 19:22:28 -0300

    private page
    With this feature I can deprive other browsing pages, so only those who are logged in with their Gmail account can access other pages.

--- controlldev/src/app/dashboard/page.tsx
+++ controlldev/src/app/dashboard/page.tsx
@@ -1,7 +1,18 @@
 import { Container } from "@/components/container"
+import { getServerSession } from "next-auth"
+import { authOptions }  from "@/lib/auth"
+import { redirect } from "next/navigation"
 
 
-export default function Dashboard() {
+
+export default async function Dashboard() {
+    const session = await getServerSession(authOptions)
+
+    if(!session || !session.user) {
+        redirect("/")
+    }
+
+
     return (
         <Container>
             <h1>Página Dashboard</h1>
