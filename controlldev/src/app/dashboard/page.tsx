9dfe734aebb3af3ab5752439b8c30e12f4b711b6
Pablo Teodoro <pabloteadoro123@gmail.com>
2025-05-21 22:50:34 -0300

    More routes were created for customers logged in with gmail, where I added a header and placed a static page to show customer tickets

--- controlldev/src/app/dashboard/page.tsx
+++ controlldev/src/app/dashboard/page.tsx
@@ -2,6 +2,8 @@ import { Container } from "@/components/container"
 import { getServerSession } from "next-auth"
 import { authOptions }  from "@/lib/auth"
 import { redirect } from "next/navigation"
+import { TicketItem } from "./components/ticket"
+import Link from "next/link"
 
 
 
@@ -15,7 +17,37 @@ export default async function Dashboard() {
 
     return (
         <Container>
-            <h1>Página Dashboard</h1>
+            <main className="mt-9 mb-2">
+                <div className="flex items-center justify-between">
+                <h1 className="text-3xl font-bold">Chamados</h1> 
+                <Link href="/dashboard/new" className="bg-blue-500 px-4 py-1 rounded text white">
+                    Solicitar novo Chamado
+                </Link>
+            </div>
+
+            <table className="min-w-full my-2">
+                <thead>
+                    <tr>
+                        <th className="font-medium text-left pl-1">CLIENTE</th>
+                        <th className="font-medium text-left p-2">DATA</th>
+                        <th className="font-medium text-left p-2">STATUS</th>
+                        <th className="font-medium text-left p-2">#</th>
+                    </tr>
+                </thead>
+                <tbody>
+                
+                    <TicketItem/>
+
+                </tbody>
+
+
+
+            </table>
+
+
+            </main>
+          
+
         </Container>
     )
 }
