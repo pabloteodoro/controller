5f4a193529891e0e312a3a9e2fabd1e61386008b
Pablo Teodoro <pabloteadoro123@gmail.com>
2025-05-27 23:57:34 -0300

    I created a form input with regex rules to register customer data

--- controlldev/src/app/dashboard/customer/components/form/index.tsx
+++ controlldev/src/app/dashboard/customer/components/form/index.tsx
@@ -3,6 +3,7 @@
 import { useForm } from "react-hook-form"
 import { z } from "zod"
 import { zodResolver } from "@hookform/resolvers/zod"
+import { Input } from "@/app/dashboard/components/input"
 
 const schema = z.object({
     name: z.string().min(1, "O Campo Nome é Obrigatório"),
@@ -22,12 +23,79 @@ export function NewCustomerForm() {
     const { register, handleSubmit, formState: { errors } } = useForm<FormData>({
         resolver: zodResolver(schema)
     })
+
+    function handleRegisterCustomer(data: FormData) {
+        console.log(data);
+    }
+
+
     return (
-        <form>
-            <label>
+        <form className="flex flex-col mt-6" onSubmit={handleSubmit(handleRegisterCustomer)}>
+            <label className="mb-1 text-lg font-medium">
                 Nome Completo:
             </label>
-            <input type="text" placeholder="Digite o nome completo..."/>
+            <Input
+            type="text"
+            name="name"
+            placeholder="Digite o Nome Completo"
+            error={errors.name?.message}
+            register={register}
+
+            
+            
+            />
+            <section className="flex gap-2 mt-2 flex-col sm:flex-row">
+                <div className="flex-1">
+                    
+                    <label className="mb-1 text-lg font-medium">
+                        Telefone:
+                    </label>
+                    <Input
+                        type="number"
+                        name="phone"
+                        placeholder="Digite o telefone"
+                        error={errors.phone?.message}
+                        register={register}
+                    />  
+                </div>
+            </section>
+
+         
+                <div className="flex-1">
+                    
+                    <label className="mb-1 text-lg font-medium">
+                        E-mail:
+                    </label>
+                    <Input
+                        type="email"
+                        name="email"
+                        placeholder="Digite o E-mail..."
+                        error={errors.email?.message}
+                        register={register}
+                    />  
+                </div>
+
+                  <label className="mb-1 text-lg font-medium">
+                        Endereço Completo:
+                    </label>
+                    <Input
+                        type="text"
+                        name="address"
+                        placeholder="Digite o Endereço..."
+                        error={errors.address?.message}
+                        register={register}
+                    /> 
+
+                    <button type="submit" className="bg-blue-500 my-4 px-2 h-11 rounded text-white font-bold">
+                        Cadastrar
+                    </button>
+            
+      
+          
+
+            
+            
+            
         </form>
     )
 }
