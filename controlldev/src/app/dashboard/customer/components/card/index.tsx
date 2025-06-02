8c610072d4155914014be9b9b4b59918722a73ce
Pablo Teodoro <pabloteadoro123@gmail.com>
2025-06-02 19:36:24 -0300

    I ended up creating a feature that only lists customers registered in the database on the customer page and I also added a refresh to the page so that nextjs can refresh the backend at the same time as returning to the previous page, so it will always be up to date.

--- controlldev/src/app/dashboard/customer/components/card/index.tsx
+++ controlldev/src/app/dashboard/customer/components/card/index.tsx
@@ -1,14 +1,14 @@
+import { CustomerProps } from "@/utils/customer.type"
 
-
-export function CardCustomer() {
+export function CardCustomer({customer}: {customer: CustomerProps}) {
     return (
     <article className="flex flex-col bg-gray-100 border-2 p-2 rounded-lg gap-2 hover:scale-105 duration-300">
             <h2>
-               <a className="font-bold"></a> Nome: João Silva
+               <a className="font-bold"></a> {customer.name}
 
             </h2>
-            <p><a className="font-bold">Email:</a> teste@teste.com</p>
-            <p className="font-bold"><a>Telefone:</a> +55 11 981693-226</p>
+            <p><a className="font-bold">Email:</a> {customer.email}</p>
+            <p className="font-bold"><a>Telefone:</a> {customer.phone}</p>
             <button className="bg-red-500 px-4 rounded text-white mt-2 self-end">
             Deletar
             </button>
--- controlldev/src/app/dashboard/customer/page.tsx
+++ controlldev/src/app/dashboard/customer/page.tsx
@@ -4,6 +4,7 @@ import { authOptions } from "@/lib/auth";
 import { redirect } from "next/navigation"; 
 import Link from 'next/link'
 import { CardCustomer } from "./components/card";
+import prismaClient from "@/lib/prisma";
 
 export default async function Customer() {
 
@@ -12,6 +13,13 @@ export default async function Customer() {
         if(!session || !session.user) {
             redirect("/")
         }
+
+        const customers = await prismaClient.customer.findMany({
+            where: {
+                userId: session.user.id
+        
+            }
+        })
     
     return (
         <Container>
@@ -27,11 +35,10 @@ export default async function Customer() {
 
                 <section className="grid grid-cols-21 sm:grid-cols-2 lg:grid-cols-3 gap-3 mt-2">
 
-                    <CardCustomer/>
+                    {customers.map(customer => (
+                        <CardCustomer key={customer.id} customer={customer}/>
+                    ))}
 
-                    <CardCustomer/>
-
-                    <CardCustomer/>
 
                 </section>
 
